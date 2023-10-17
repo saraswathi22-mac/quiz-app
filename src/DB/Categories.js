322acbe6d2009a42d53ab2ffb280938e7a3315b9
exit <saraswathi.mac@gmail.com>
2023-10-17 17:07:43 +0530

    Added the categories of quiz

---
+++ src/DB/Categories.js
@@ -0,0 +1,24 @@
+const Categories = [
+  {
+    category: "General Knowledge",
+    value: 9,
+  },
+  { category: "Books", value: 10 },
+  { category: "Films", value: 11 },
+  { category: "Music", value: 12 },
+  { category: "Musicals and Theaters", value: 13 },
+  { category: "Television", value: 14 },
+  { category: "Science and Nature", value: 17 },
+  { category: "Computer", value: 18 },
+  { category: "Mathematics", value: 19 },
+  { category: "Sports", value: 21 },
+  { category: "Geography", value: 22 },
+  { category: "History", value: 23 },
+  { category: "Animals", value: 27 },
+  { category: "Vehicles", value: 28 },
+  { category: "Comics", value: 29 },
+  { category: "Gadgets", value: 30 },
+  { category: "Cartoon and Animations", value: 32 },
+];
+
+export default Categories;
