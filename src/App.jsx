c34b20a066301b84fb8e579d4073afea9bcc40bc
exit <saraswathi.mac@gmail.com>
2023-10-17 16:37:49 +0530

    Updated Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
+import {useState} from "react"
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 
 function App() {
+  const [name, setName] = useState();
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home name={name} setName={setName} />} />
       </Routes>
     </Router>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { TextField } from "@mui/material";
+import "./Home.css";
 
-const Home = () => {
+const Home = ({ name, setName }) => {
   return (
-    <div>
-        Home
+    <div className="content">
+      <div className="settings">
+        <span style={{ fontSize: 30 }}>
+          Please fill the details to start the quiz
+        </span>
+        <div className="settings__select">
+          <TextField
+            style={{ marginBottom: 25 }}
+            label="Enter Your Name"
+            variant="outlined"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
