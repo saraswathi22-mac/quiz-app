36a42a93a141d67335cec5343c0a4844e4c8aff0
exit <saraswathi.mac@gmail.com>
2023-10-18 18:38:55 +0530

    Created Quiz component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Quiz from "./components/Quiz/Quiz";
 import Header from "./components/Header/Header";
 import { useFetchQuestions } from "./hooks/useFetchQuestions";
 
@@ -21,6 +22,7 @@ function App() {
             />
           }
         />
+        <Route path="/quiz" element={<Quiz name={name} />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Quiz = ({name}) => {
+  return (
+    <div>
+        <span>Welcome, {name}</span>
+    </div>
+  )
+}
+
+export default Quiz
