0d8045b8974d93e09e6d59c41a32707a3b8a3298
exit <saraswathi.mac@gmail.com>
2023-12-11 21:24:37 +0530

    Updated Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <h1 className="text-6xl font-bold underline text-center p-5">Quizzle</h1>
+  );
+};
+
+export default Header;
--- src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <div>
-      <h1>Quiz App</h1>
-    </div>
-  );
-};
-
-export default Header;
