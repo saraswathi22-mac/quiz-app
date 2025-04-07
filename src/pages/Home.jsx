470f87800d1ac6de892ace58e9f797872eba4131
Sridhar Guna <sridharguna@Sridhars-Laptop.local>
2025-04-07 17:32:47 +0530

    Updated the main screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,82 +1,46 @@
 import { TextField, MenuItem, Button } from "@mui/material";
 import { useState } from "react";
-import Categories from "../DB/Categories";
 import { useNavigate } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
 
-const Home = ({ name, setName, fetchQuestions }) => {
-  const [category, setCategory] = useState("");
-  const [difficulty, setDifficulty] = useState("");
+const Home = ({ name, setName }) => {
   const [error, setError] = useState(false);
 
   const navigate = useNavigate();
 
   const handleSubmit = () => {
-    if (!category || !difficulty || !name) {
+    if (!name) {
       setError(true);
       return;
     } else {
       setError(false);
-      fetchQuestions(category, difficulty);
-      navigate("/quiz");
+      navigate("/rules-and-selection");
     }
   };
 
   return (
     <div className="flex justify-around">
       <div className="flex flex-col items-center p-2.5 w-[45%] font-light font-serif">
-        <span className="text-3xl">
-          Please fill the details to start the quiz
-        </span>
+        <span className="text-2xl">Play to gain knowledge and have fun!</span>
         <div className="flex flex-col justify-evenly w-full text-left flex-0.8 p-5">
-          {error && <ErrorMessage>Please fill all the fields.</ErrorMessage>}
+          {error && (
+            <ErrorMessage>Please enter your name to proceed.</ErrorMessage>
+          )}
           <TextField
             style={{ marginBottom: 25 }}
             label="Enter Your Name"
             variant="outlined"
             onChange={(e) => setName(e.target.value)}
           />
-          <TextField
-            select
-            label="Select Category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            variant="outlined"
-            style={{ marginBottom: 30 }}
-          >
-            {Categories.map((item) => (
-              <MenuItem key={item.category} value={item.value}>
-                {item.category}
-              </MenuItem>
-            ))}
-          </TextField>
-          <TextField
-            select
-            label="Select Difficulty"
-            value={difficulty}
-            onChange={(e) => setDifficulty(e.target.value)}
-            variant="outlined"
-            style={{ marginBottom: 30 }}
-          >
-            <MenuItem key="Easy" value="easy">
-              Easy
-            </MenuItem>
-            <MenuItem key="Medium" value="medium">
-              Medium
-            </MenuItem>
-            <MenuItem key="Hard" value="hard">
-              Hard
-            </MenuItem>
-          </TextField>
-          <Button
-            variant="contained"
-            color="secondary"
-            size="large"
-            onClick={handleSubmit}
-          >
-            Start Quiz
-          </Button>
         </div>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="large"
+          onClick={handleSubmit}
+        >
+          Ready to test your knowledge?
+        </Button>
       </div>
     </div>
   );
