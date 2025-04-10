b9b13ba6d7f43dcf7aab151a73fae8fef799cbbc
Sridhar Guna <sridharguna@Sridhars-Laptop.local>
2025-04-10 23:42:33 +0530

    Updated the UI of app screen

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="relative bg-gradient-to-r from-green-300 to-blue-400 w-full h-screen flex justify-center items-center">
-      <div className="absolute box-border size-32 w-[75%] h-[80%] border-4 p-4 bg-gradient-to-r from-green-300 to-blue-400 text-white p-4 rounded-lg shadow-lg shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+      <div className="absolute box-border size-32 w-[75%] h-[80%] border-4 p-4 bg-gradient-to-r from-green-300 to-blue-400  p-4 rounded-lg shadow-lg shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
         <App />
       </div>
     </div>
