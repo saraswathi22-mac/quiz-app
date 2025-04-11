d7582e4365eeae42fc08219526de22e28cf669a7
Sridhar Guna <sridharguna@Sridhars-Laptop.local>
2025-04-11 18:48:41 +0530

    Updated the Results page

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -12,13 +12,27 @@ const Result = ({ name, score }) => {
   }, [name, navigate]);
 
   return (
-    <div className="flex flex-col justify-center text-center h-[60vh]">
-      <span className="text-bold text-3xl">
-        {name}, your score is {score}
-      </span>
+    <div className="flex flex-col justify-center items-center text-center h-[60vh]">
+      <span className="text-bold text-3xl mb-5">Your score:</span>
+      <div className="flex h-32 w-32 items-center justify-center rounded-full bg-cyan-300 text-white text-5xl">
+        <div className="flex h-28 w-28 items-center justify-center rounded-full bg-cyan-400">
+          <div className="flex h-24 w-24 items-center justify-center rounded-full bg-cyan-500">
+            <p>{score} </p>
+          </div>
+        </div>
+      </div>
+      <div className="mt-5 mb-10 text-2xl text-gray-700">
+        {(score === 10)
+          ? `Boom! Perfect Score, ${name} – You’re unstoppable!`
+          : (score > 6)
+          ? `You're doing amazing, ${name}. Keep shining!.`
+          : (score > 3)
+          ? `Almost there, ${name} — let's push it up a notch!.`
+          : `Better luck next time ${name}.`}
+      </div>
       <Button
         variant="contained"
-        color="secondary"
+        color="primary"
         size="large"
         className="self-center"
         href="/"
