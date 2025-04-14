9e14ac8bad8862644219e60317911c6d56b64290
M A Saraswathi <saraswathi.mac@gmail.com>
2025-04-14 21:35:48 +0530

    Updated Quiz pages

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -49,45 +49,54 @@ const Question = ({
 
   return (
     <div className="flex flex-col w-full items-center">
-      <h1>Question {currQues + 1} :</h1>
-      <div className="flex flex-col items-center justify-around p-5 min-h-[350px] w-[95%] mt-2.5 border-solid border-4 border-slate-400">
-        <h2>{questions[currQues]?.question}</h2>
-        <div className="flex w-full items-center justify-around flex-1 flex-wrap m-4">
-          {error && <ErrorMessage>{error}</ErrorMessage>}
-          {options &&
-            options.map((i) => (
-              <button
-                className={`m-2.5 py-[15px] px-[20px] h-12 w-full sm:w-[46%] border-2 border-solid border-slate-300 shadow-lg  ${
-                  selected && handleSelect(i)
-                }`}
-                key={i}
-                onClick={() => handleCheck(i)}
-                disabled={selected}
-              >
-                {i}
-              </button>
-            ))}
-        </div>
-        <div className="flex w-full justify-around">
-          <Button
-            variant="contained"
-            color="secondary"
-            size="large"
-            style={{ width: 185 }}
-            href="/"
-            onClick={() => handleQuit()}
-          >
-            Quit
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            style={{ width: 185 }}
-            onClick={handleNext}
-          >
-            {currQues > 20 ? "Submit" : "Next Question"}
-          </Button>
+      <div className="p-[3px] rounded-xl bg-gradient-to-r from-green-300 to-blue-400 w-[95%] mt-2.5">
+        <div className="flex flex-col items-center justify-around p-5 min-h-[350px] w-full rounded-xl bg-white">
+          <div className="text-xl font-bold text-gray-500 mb-4 tracking-wide">
+            Question {currQues + 1}
+          </div>
+          <div className="text-3xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-400 to-purple-500 text-center mb-6">
+            {questions[currQues]?.question}
+          </div>
+
+          <div className="flex w-full items-center justify-around flex-1 flex-wrap m-4 gap-5">
+            {error && <ErrorMessage>{error}</ErrorMessage>}
+            {options &&
+              options.map((i) => (
+                <button
+                  className={`m-2 py-4 px-6 w-full md:w-[55%] text-lg font-semibold rounded-full shadow-md border-2 transition-all  ease-in-out border-transparent hover:scale-105  
+          hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500 focus:outline-none focus:ring-blue-300 ${
+            selected && handleSelect(i)
+          }`}
+                  key={i}
+                  onClick={() => handleCheck(i)}
+                  disabled={selected}
+                >
+                  {i}
+                </button>
+              ))}
+          </div>
+
+          <div className="flex w-full justify-around">
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              style={{ width: 185 }}
+              href="/"
+              onClick={() => handleQuit()}
+            >
+              Quit
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              style={{ width: 185 }}
+              onClick={handleNext}
+            >
+              {currQues + 1 === 10 ? "Submit" : "Next"}
+            </Button>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -22,13 +22,16 @@ const Quiz = ({ questions, score, setScore, setQuestions }) => {
 
   return (
     <div className="flex flex-col items-center font-sans">
-      
       {questions ? (
         <>
-          <div className="w-full flex justify-between uppercase m-2.5">
-            <span>{questions[currQues]?.category}</span>
-            <span>Score : {score}</span>
+          <div className="p-1 inline-block bg-gradient-to-r from-green-400 via-teal-400 to-blue-500 rounded-lg">
+            <div className="bg-white px-4 py-2 rounded-lg">
+              <h1 className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-600 to-blue-600">
+                <span>{questions[currQues]?.category}</span>
+              </h1>
+            </div>
           </div>
+
           <Question
             currQues={currQues}
             setCurrQues={setCurrQues}
